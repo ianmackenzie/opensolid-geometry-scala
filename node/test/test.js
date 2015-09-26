e3a5e92c21d0e94c0a49442179275e14cd21ab0f
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2015-09-27 01:56:42 +1000

    Add temporary hacky test file

---
+++ node/test/test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let opensolid = require('..');
+let Interval = opensolid.Interval;
+let Point2d = opensolid.Point2d;
+let Handedness = opensolid.Handedness;
+
+opensolid.addTolerantComparisonsTo(Number);
+
+console.log('=== interval ===');
+let interval = Interval(2, 3);
+console.log(interval.median);
+
+console.log('=== singleton ===');
+let singleton = Interval.singleton(5);
+console.log(singleton.lowerBound);
+console.log(singleton.upperBound);
+
+console.log('=== comparisons ===');
+let tiny = 1e-14;
+let small = 1e-8;
+console.log(tiny.isZero());
+console.log(small.isZero());
+console.log(small.isZero(1e-6));
