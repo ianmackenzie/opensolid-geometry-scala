9e5e6f7ba3f7835ddcaf7cb62c441a59e711e177
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2015-09-28 15:30:35 +1000

    Switch from 'let' to 'const' in JavaScript code

--- node/test/test.js
+++ node/test/test.js
@@ -1,27 +1,27 @@
 'use strict';
 
-let opensolid = require('..');
-let Interval = opensolid.Interval;
-let Point2d = opensolid.Point2d;
-let Handedness = opensolid.Handedness;
-let Sign = opensolid.Sign;
-let Vector2d = opensolid.Vector2d;
-let UnitVector2d = opensolid.UnitVector2d;
+const opensolid = require('..');
+const Interval = opensolid.Interval;
+const Point2d = opensolid.Point2d;
+const Handedness = opensolid.Handedness;
+const Sign = opensolid.Sign;
+const Vector2d = opensolid.Vector2d;
+const UnitVector2d = opensolid.UnitVector2d;
 
 opensolid.addTolerantComparisonsTo(Number);
 
 console.log('=== interval ===');
-let interval = Interval(2, 3);
+const interval = Interval(2, 3);
 console.log(interval.median);
 
 console.log('=== singleton ===');
-let singleton = Interval.singleton(5);
+const singleton = Interval.singleton(5);
 console.log(singleton.lowerBound);
 console.log(singleton.upperBound);
 
 console.log('=== comparisons ===');
-let tiny = 1e-14;
-let small = 1e-8;
+const tiny = 1e-14;
+const small = 1e-8;
 console.log(tiny.isZero());
 console.log(small.isZero());
 console.log(small.isZero(1e-6));
@@ -33,7 +33,7 @@ console.log(Sign.POSITIVE instanceof Handedness);
 console.log(Sign.POSITIVE instanceof Sign);
 
 console.log('=== unit vectors ===');
-let e0 = UnitVector2d(1, 0);
+const e0 = UnitVector2d(1, 0);
 console.log(e0 instanceof UnitVector2d);
 console.log(e0 instanceof Vector2d);
 console.log(e0.squaredLength);
