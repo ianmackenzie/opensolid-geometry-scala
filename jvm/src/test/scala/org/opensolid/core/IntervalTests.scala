007879f8b9f489b61f20b91b9e60ef0c4f82a40c
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2015-10-28 17:19:13 +1100

    Simplify tests a bit
    Get rid of unnecessary Double generators

--- jvm/src/test/scala/org/opensolid/core/IntervalTests.scala
+++ jvm/src/test/scala/org/opensolid/core/IntervalTests.scala
@@ -3,29 +3,21 @@ package org.opensolid.core
 import org.scalacheck._
 import org.scalacheck.Prop.BooleanOperators
 
-object DoubleGenerators {
-  val finiteDouble: Gen[Double] = Gen.chooseNum(-1e-6, 1e6)
-
-  val zeroToOne: Gen[Double] = Gen.chooseNum(0.0, 1.0)
-}
-
 object IntervalGenerators {
-  import DoubleGenerators._
-
-  val singletonInterval: Gen[Interval] = finiteDouble.map(Interval(_))
+  val singletonInterval: Gen[Interval] = Arbitrary.arbitrary[Double].map(Interval(_))
 
   val closedInterval: Gen[Interval] = for {
-    firstValue <- finiteDouble
-    secondValue <- finiteDouble
+    firstValue <- Arbitrary.arbitrary[Double]
+    secondValue <- Arbitrary.arbitrary[Double]
   } yield Interval(firstValue.min(secondValue), firstValue.max(secondValue))
 
   val negativeHalfOpenInterval: Gen[Interval] =
-    finiteDouble.map(Interval(Double.NegativeInfinity, _))
+    Arbitrary.arbitrary[Double].map(Interval(Double.NegativeInfinity, _))
 
   val positiveHalfOpenInterval: Gen[Interval] =
-    finiteDouble.map(Interval(_, Double.PositiveInfinity))
+    Arbitrary.arbitrary[Double].map(Interval(_, Double.PositiveInfinity))
 
-  val anyInterval: Gen[Interval] = Gen.frequency(
+  implicit val arbitraryInterval: Arbitrary[Interval] = Arbitrary(Gen.frequency(
     1 -> Interval.Empty,
     1 -> Interval.Whole,
     1 -> Interval(0.0),
@@ -34,18 +26,15 @@ object IntervalGenerators {
     2 -> negativeHalfOpenInterval,
     2 -> positiveHalfOpenInterval,
     2 -> singletonInterval,
-    10 -> closedInterval)
-
-  implicit val arbitraryInterval: Arbitrary[Interval] = Arbitrary(anyInterval)
+    10 -> closedInterval))
 }
 
 object IntervalSpec extends Properties("Interval") {
-  import DoubleGenerators._
   import IntervalGenerators._
 
   property("equals") = Prop.forAll {(interval: Interval) => interval == interval}
 
-  property("interpolated") = Prop.forAll(closedInterval, zeroToOne) {
+  property("interpolated") = Prop.forAll(closedInterval, Gen.chooseNum(0.0, 1.0)) {
     (interval: Interval, value: Double) => {
       val interpolated = interval.interpolated(value)
       interval.contains(interpolated, 2.0 * Interval.ulp(interval))
