c01ca7d4bafa369444b3ede972a3cbd1ce6a44c7
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2015-10-29 12:01:22 +1100

    Improve Interval tests style and coverage
    - Rename IntervalTests to IntervalProperties
    - Rename test methods to appear in the output as methods/method
    overloads
    - Improve interval generators by explicitly generating intervals of 1
    ulp/2 ulp widths, and adjusting generic generator to more often generate
    small intervals
    - Tighten split point requirements on Interval.bisected

--- jvm/src/test/scala/org/opensolid/core/IntervalProperties.scala
+++ jvm/src/test/scala/org/opensolid/core/IntervalProperties.scala
@@ -6,16 +6,28 @@ import org.scalacheck.Prop.BooleanOperators
 object IntervalGenerators {
   val singletonInterval: Gen[Interval] = Arbitrary.arbitrary[Double].map(Interval(_))
 
-  val closedInterval: Gen[Interval] = for {
-    firstValue <- Arbitrary.arbitrary[Double]
-    secondValue <- Arbitrary.arbitrary[Double]
-  } yield Interval(firstValue.min(secondValue), firstValue.max(secondValue))
+  val randomInterval: Gen[Interval] = for {
+    median <- Arbitrary.arbitrary[Double]
+    halfWidth <- Arbitrary.arbitrary[Double].retryUntil(_ >= 0.0)
+  } yield Interval(median - halfWidth, median + halfWidth)
 
   val negativeHalfOpenInterval: Gen[Interval] =
     Arbitrary.arbitrary[Double].map(Interval(Double.NegativeInfinity, _))
 
   val positiveHalfOpenInterval: Gen[Interval] =
     Arbitrary.arbitrary[Double].map(Interval(_, Double.PositiveInfinity))
+
+  val oneUlpInterval: Gen[Interval] =
+    Arbitrary.arbitrary[Double].map(value => Interval(value, value + math.ulp(value)))
+
+  val twoUlpInterval: Gen[Interval] =
+    Arbitrary.arbitrary[Double].map(
+      value => Interval(value - math.ulp(value), value + math.ulp(value)))
+
+  val closedInterval: Gen[Interval] = Gen.frequency(
+    1 -> oneUlpInterval,
+    1 -> twoUlpInterval,
+    10 -> randomInterval)
 
   implicit val arbitraryInterval: Arbitrary[Interval] = Arbitrary(Gen.frequency(
     1 -> Interval.Empty,
@@ -26,22 +38,25 @@ object IntervalGenerators {
     2 -> negativeHalfOpenInterval,
     2 -> positiveHalfOpenInterval,
     2 -> singletonInterval,
-    10 -> closedInterval))
+    2 -> oneUlpInterval,
+    2 -> twoUlpInterval,
+    10 -> randomInterval))
 }
 
-object IntervalSpec extends Properties("Interval") {
+object IntervalProperties extends Properties("Interval") {
   import IntervalGenerators._
 
-  property("equals") = Prop.forAll {(interval: Interval) => interval == interval}
+  property("equals(other)") = Prop.forAll {(interval: Interval) => interval == interval} &&
+    Prop.forAll {(interval: Interval, value: Double) => interval != value}
 
-  property("interpolated") = Prop.forAll(closedInterval, Gen.chooseNum(0.0, 1.0)) {
+  property("interpolated(value)") = Prop.forAll(closedInterval, Gen.chooseNum(0.0, 1.0)) {
     (interval: Interval, value: Double) => {
       val interpolated = interval.interpolated(value)
       interval.contains(interpolated, 2.0 * Interval.ulp(interval))
     }
   }
 
-  property("randomValue") = Prop.forAll(closedInterval) {
+  property("randomValue()") = Prop.forAll(closedInterval) {
     (interval: Interval) => {
       val randomValue = interval.randomValue()
       interval.contains(randomValue, 2.0 * Interval.ulp(interval))
@@ -57,22 +72,24 @@ object IntervalSpec extends Properties("Interval") {
         val basicChecks = (lower.upperBound == upper.lowerBound) &&
           (lower.lowerBound == interval.lowerBound) &&
           (upper.upperBound == interval.upperBound)
+
         val mid = lower.upperBound
-        
-        if (interval.width.isZero(DefaultPrecision)) {
-          basicChecks &&
-            mid >= interval.lowerBound &&
-            mid <= interval.upperBound
-        } else {
+        if (interval.lowerBound + math.ulp(interval.lowerBound) < interval.upperBound) {
+          // There is enough room for a bisection point distinct from both lower and upper bounds
           basicChecks &&
             mid > interval.lowerBound &&
             mid < interval.upperBound
+        } else {
+          // Very small interval - bisection point may be equal to lower and/or upper bounds
+          basicChecks &&
+            mid >= interval.lowerBound &&
+            mid <= interval.upperBound
         }
       }
     }
   }
 
-  property("hull(Double)") = Prop.forAll {
+  property("hull(value)") = Prop.forAll {
     (interval: Interval, value: Double) => {
       val hull = interval.hull(value)
 
@@ -84,7 +101,7 @@ object IntervalSpec extends Properties("Interval") {
     }
   }
 
-  property("hull(Interval)") = Prop.forAll {
+  property("hull(that)") = Prop.forAll {
     (firstInterval: Interval, secondInterval: Interval) => {
       val hull = firstInterval.hull(secondInterval)
       
@@ -98,7 +115,7 @@ object IntervalSpec extends Properties("Interval") {
     }
   }
 
-  property("intersection") = Prop.forAll(
+  property("intersection(that)") = Prop.forAll(
     (firstInterval: Interval, secondInterval: Interval) => {
       val intersection = firstInterval.intersection(secondInterval)
 
