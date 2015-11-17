6cd2227b0395a775cff82639e5422bdca1ea2bab
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2015-11-17 11:13:23 +1100

    Add more operations to Box types

--- shared/src/main/scala/org/opensolid/core/Box2d.scala
+++ shared/src/main/scala/org/opensolid/core/Box2d.scala
@@ -10,6 +10,50 @@ final case class Box2d(x: Interval, y: Interval) extends Bounded2d {
   }
 
   override def bounds: Box2d = this
+
+  def isEmpty: Boolean = x.isEmpty || y.isEmpty
+
+  def isWhole: Boolean = x.isWhole && y.isWhole
+
+  def isSingleton: Boolean = x.isSingleton && y.isSingleton
+
+  def hull(point: Point2d): Box2d = Box2d(x.hull(point.x), y.hull(point.y))
+
+  def hull(that: Box2d): Box2d = Box2d(this.x.hull(that.x), this.y.hull(that.y))
+
+  def intersection(that: Box2d) = {
+    val x = this.x.intersection(that.x)
+    val y = this.y.intersection(that.y)
+    if (x.isEmpty || y.isEmpty) Box2d.Empty else Box2d(x, y)
+  }
+
+  def overlaps(that: Box2d): Boolean = this.x.overlaps(that.x) && this.y.overlaps(that.y)
+
+  def overlaps(that: Box2d, tolerance: Double): Boolean =
+    this.x.overlaps(that.x, tolerance) && this.y.overlaps(that.y, tolerance)
+
+  def contains(point: Point2d): Boolean = x.contains(point.x) && y.contains(point.y)
+
+  def contains(point: Point2d, tolerance: Double): Boolean =
+    x.contains(point.x, tolerance) && y.contains(point.y, tolerance)
+
+  def contains(that: Box2d): Boolean = this.x.contains(that.x) && this.y.contains(that.y)
+
+  def contains(that: Box2d, tolerance: Double): Boolean =
+    this.x.contains(that.x, tolerance) && this.y.contains(that.y, tolerance)
+
+  def +(vector: Vector2d): Box2d = Box2d(x + vector.x, y + vector.y)
+
+  def +(vectorBox: VectorBox2d): Box2d = Box2d(x + vectorBox.x, y + vectorBox.y)
+
+  def -(vector: Vector2d): Box2d = Box2d(x - vector.x, y - vector.y)
+
+  def -(vectorBox: VectorBox2d): Box2d = Box2d(x - vectorBox.x, y - vectorBox.y)
+
+  def -(point: Point2d): VectorBox2d = VectorBox2d(x - point.x, y - point.y)
+
+  def -(that: Box2d): VectorBox2d = VectorBox2d(this.x - that.x, this.y - that.y)
+
 }
 
 object Box2d {
--- shared/src/main/scala/org/opensolid/core/Box3d.scala
+++ shared/src/main/scala/org/opensolid/core/Box3d.scala
@@ -11,6 +11,60 @@ final case class Box3d(x: Interval, y: Interval, z: Interval) extends Bounded3d 
   }
 
   override def bounds: Box3d = this
+
+  def isEmpty: Boolean = x.isEmpty || y.isEmpty || z.isEmpty
+
+  def isWhole: Boolean = x.isWhole && y.isWhole && z.isWhole
+
+  def isSingleton: Boolean = x.isSingleton && y.isSingleton && z.isSingleton
+
+  def hull(point: Point3d): Box3d = Box3d(x.hull(point.x), y.hull(point.y), z.hull(point.z))
+
+  def hull(that: Box3d): Box3d =
+    Box3d(this.x.hull(that.x), this.y.hull(that.y), this.z.hull(that.z))
+
+  def intersection(that: Box3d) = {
+    val x = this.x.intersection(that.x)
+    val y = this.y.intersection(that.y)
+    val z = this.z.intersection(that.z)
+    if (x.isEmpty || y.isEmpty || z.isEmpty) Box3d.Empty else Box3d(x, y, z)
+  }
+
+  def overlaps(that: Box3d): Boolean =
+    this.x.overlaps(that.x) && this.y.overlaps(that.y) && this.z.overlaps(that.z)
+
+  def overlaps(that: Box3d, tolerance: Double): Boolean =
+    this.x.overlaps(that.x, tolerance) &&
+    this.y.overlaps(that.y, tolerance) &&
+    this.z.overlaps(that.z, tolerance)
+
+  def contains(point: Point3d): Boolean =
+    x.contains(point.x) && y.contains(point.y) && z.contains(point.z)
+
+  def contains(point: Point3d, tolerance: Double): Boolean =
+    x.contains(point.x, tolerance) &&
+    y.contains(point.y, tolerance) &&
+    z.contains(point.z, tolerance)
+
+  def contains(that: Box3d): Boolean =
+    this.x.contains(that.x) && this.y.contains(that.y) && this.z.contains(that.z)
+
+  def contains(that: Box3d, tolerance: Double): Boolean =
+    this.x.contains(that.x, tolerance) &&
+    this.y.contains(that.y, tolerance) &&
+    this.z.contains(that.z, tolerance)
+
+  def +(vector: Vector3d): Box3d = Box3d(x + vector.x, y + vector.y, z + vector.z)
+
+  def +(vectorBox: VectorBox3d): Box3d = Box3d(x + vectorBox.x, y + vectorBox.y, z + vectorBox.z)
+
+  def -(vector: Vector3d): Box3d = Box3d(x - vector.x, y - vector.y, z - vector.z)
+
+  def -(vectorBox: VectorBox3d): Box3d = Box3d(x - vectorBox.x, y - vectorBox.y, z - vectorBox.z)
+
+  def -(point: Point3d): VectorBox3d = VectorBox3d(x - point.x, y - point.y, z - point.z)
+
+  def -(that: Box3d): VectorBox3d = VectorBox3d(this.x - that.x, this.y - that.y, this.z - that.z)
 }
 
 object Box3d {
