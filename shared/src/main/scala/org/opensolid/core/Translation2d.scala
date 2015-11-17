b74a4c4c26388777e06118b64a46b15f8d19fcdf
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2015-11-18 10:07:02 +1100

    Add MPL-2.0 license to all source files

--- shared/src/main/scala/org/opensolid/core/Translation2d.scala
+++ shared/src/main/scala/org/opensolid/core/Translation2d.scala
@@ -1,3 +1,17 @@
+/*******************************************************************************
+*                                                                              *
+*  OpenSolid is a generic library for the representation and manipulation of   *
+*  geometric objects such as points, curves, surfaces, and volumes.            *
+*                                                                              *
+*  Copyright 2007-2015 by Ian Mackenzie                                        *
+*  ian.e.mackenzie@gmail.com                                                   *
+*                                                                              *
+*  This Source Code Form is subject to the terms of the Mozilla Public         *
+*  License, v. 2.0. If a copy of the MPL was not distributed with this file,   *
+*  you can obtain one at http://mozilla.org/MPL/2.0/.                          *
+*                                                                              *
+*******************************************************************************/
+
 package org.opensolid.core
 
 final case class Translation2d(vector: Vector2d) extends Transformation2d {
