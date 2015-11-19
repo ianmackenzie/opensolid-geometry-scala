0a2f051c1dc14da8dce74093a873f6a22c4c3613
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2015-11-19 11:51:56 +1100

    Add Axis*d types

---
+++ shared/src/main/scala/org/opensolid/core/Axis2d.scala
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  OpenSolid is a generic library for the representation and manipulation    //
+//  of geometric objects such as points, curves, surfaces, and volumes.       //
+//                                                                            //
+//  Copyright 2007-2015 by Ian Mackenzie                                      //
+//  ian.e.mackenzie@gmail.com                                                 //
+//                                                                            //
+//  This Source Code Form is subject to the terms of the Mozilla Public       //
+//  License, v. 2.0. If a copy of the MPL was not distributed with this file, //
+//  you can obtain one at http://mozilla.org/MPL/2.0/.                        //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+package org.opensolid.core
+
+case class Axis2d(
+  originPoint: Point2d,
+  direction: Direction2d,
+  handedness: Handedness = Handedness.Right
+) extends Transformable2d[Axis2d] with Scalable2d[Axis2d] {
+
+  def pointAt(distance: Double): Point2d = originPoint + distance * direction
+
+  def normalDirection: Direction2d = handedness.sign * direction.normalDirection
+
+  def reversed: Axis2d = Axis2d(originPoint, -direction, -handedness)
+
+  def normalAxis: Axis2d = Axis2d(originPoint, normalDirection, handedness)
+
+  def transformedBy(transformation: Transformation2d): Axis2d =
+    Axis2d(transformation(originPoint), transformation(direction), transformation(handedness))
+
+  def scaledAbout(point: Point2d, scale: Double): Axis2d = {
+    require(scale > 0.0)
+    Axis2d(originPoint.scaledAbout(point, scale), direction, handedness)
+  }
+}
+
+object Axis2d {
+  val X = Axis2d(Point2d.Origin, Direction2d.X)
+  val Y = Axis2d(Point2d.Origin, Direction2d.Y)
+}
---
+++ shared/src/main/scala/org/opensolid/core/Axis3d.scala
@@ -0,0 +1,37 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  OpenSolid is a generic library for the representation and manipulation    //
+//  of geometric objects such as points, curves, surfaces, and volumes.       //
+//                                                                            //
+//  Copyright 2007-2015 by Ian Mackenzie                                      //
+//  ian.e.mackenzie@gmail.com                                                 //
+//                                                                            //
+//  This Source Code Form is subject to the terms of the Mozilla Public       //
+//  License, v. 2.0. If a copy of the MPL was not distributed with this file, //
+//  you can obtain one at http://mozilla.org/MPL/2.0/.                        //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+package org.opensolid.core
+
+case class Axis3d(originPoint: Point3d, direction: Direction3d)
+  extends Transformable3d[Axis3d] with Scalable3d[Axis3d] {
+
+  def pointAt(distance: Double): Point3d = originPoint + distance * direction
+
+  def reversed: Axis3d = Axis3d(originPoint, -direction)
+
+  def transformedBy(transformation: Transformation3d): Axis3d =
+    Axis3d(transformation(originPoint), transformation(direction))
+
+  def scaledAbout(point: Point3d, scale: Double): Axis3d = {
+    require(scale > 0.0)
+    Axis3d(originPoint.scaledAbout(point, scale), direction)
+  }
+}
+
+object Axis3d {
+  val X = Axis3d(Point3d.Origin, Direction3d.X)
+  val Y = Axis3d(Point3d.Origin, Direction3d.Y)
+  val Z = Axis3d(Point3d.Origin, Direction3d.Z)
+}
