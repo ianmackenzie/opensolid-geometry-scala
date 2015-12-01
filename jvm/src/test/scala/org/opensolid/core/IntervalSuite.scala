b7655a7825cef2bfcb4e33aa687fd9332b6a4a8c
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2015-12-01 14:53:58 +1100

    Port last Interval tests over to ScalaTest

--- jvm/src/test/scala/org/opensolid/core/IntervalSuite.scala
+++ jvm/src/test/scala/org/opensolid/core/IntervalSuite.scala
@@ -18,6 +18,7 @@ import org.opensolid.core.IntervalGenerators._
 import org.scalacheck.Gen
 import org.scalatest._
 import org.scalatest.prop._
+import org.scalatest.matchers._
 
 class IntervalSuite extends TestSuite {
   implicit override val generatorDrivenConfig =
@@ -28,6 +29,165 @@ class IntervalSuite extends TestSuite {
     case Interval(Double.NegativeInfinity, upper) => Gen.chooseNum(upper - 1e8, upper)
     case Interval(lower, Double.PositiveInfinity) => Gen.chooseNum(lower, lower + 1e8)
     case _ => Gen.chooseNum(0.0, 1.0).map(interval.interpolated(_)).suchThat(interval.contains(_))
+  }
+
+  def expandedInterval(interval: Interval): Interval =
+    Interval(interval.lowerBound - interval.width / 2, interval.upperBound + interval.width / 2)
+
+  def intervalOverlapping(domain: Interval): Gen[Interval] = {
+    val expandedDomain = expandedInterval(domain)
+    for {
+      firstValue <- valueWithin(expandedDomain)
+      secondValue <- valueWithin(expandedDomain)
+      interval = firstValue.hull(secondValue)
+      if interval.overlaps(domain)
+    } yield interval
+  }
+
+  def testIntervalForDomain(domain: Interval): Gen[Interval] = {
+    Gen.frequency(
+      1 -> Interval.Empty,
+      1 -> Interval.Whole,
+      10 -> intervalOverlapping(domain)
+    )
+  }
+
+  def evaluateWithin(domain: Interval, unaryFunction: (Double) => Double): Gen[Double] = {
+    for {
+      x <- valueWithin(domain)
+      y = unaryFunction(x)
+    } yield y
+  }
+
+  def evaluateWithin(
+    xDomain: Interval,
+    yDomain: Interval,
+    binaryFunction: (Double, Double) => Double
+  ): Gen[Double] = {
+    for {
+      x <- valueWithin(xDomain)
+      y <- valueWithin(yDomain)
+      z = binaryFunction(x, y)
+    } yield z
+  }
+
+  def approximatelyContain(value: Double): Matcher[Interval] = new Matcher[Interval] {
+    def apply(interval: Interval): MatchResult = {
+      val result =
+        interval.contains(value) ||
+        interval.contains(value, 2 * Interval.ulp(interval).max(math.ulp(1.0))) ||
+        value.isNaN
+      MatchResult(result, s"$interval does not contain $value", s"$interval contains $value")
+    }
+  }
+
+  def testUnaryFunction(
+    scalarFunction: (Double) => Double,
+    intervalFunction: (Interval) => Interval,
+    domain: Interval = Interval.Whole
+  ): Unit = {
+    forAll(testIntervalForDomain(domain), minSuccessful(50)) {
+      (xInterval: Interval) => {
+        val yInterval = intervalFunction(xInterval)
+        if (xInterval.isEmpty) {
+          yInterval shouldBe empty
+        } else if (xInterval.isSingleton) {
+          yInterval should approximatelyContain(scalarFunction(xInterval.lowerBound))
+        } else if (yInterval.isEmpty) {
+          forAll(valueWithin(xInterval.intersection(domain)), minSuccessful(10)) {
+            (xValue: Double) => {
+              val yValue = scalarFunction(xValue)
+              assert(yValue.isInfinity || yValue.isNaN)
+            }
+          }
+        } else {
+          val yValues = evaluateWithin(xInterval.intersection(domain), scalarFunction)
+          forAll(yValues, minSuccessful(10)) {
+            (yValue: Double) => {
+              yInterval should approximatelyContain(yValue)
+            }
+          }
+        }
+      }
+    }
+  }
+
+  def testMixedFunction(
+    scalarFunction: (Double, Double) => Double,
+    intervalFunction: (Interval, Double) => Interval,
+    xDomain: Interval = Interval.Whole,
+    yDomain: Interval = Interval.Whole
+  ): Unit = {
+    forAll(testIntervalForDomain(xDomain), valueWithin(yDomain), minSuccessful(50)) {
+      (xInterval: Interval, yValue: Double) => {
+        val zInterval = intervalFunction(xInterval, yValue)
+        if (xInterval.isEmpty) {
+          zInterval shouldBe empty
+        } else if (xInterval.isSingleton) {
+          zInterval should approximatelyContain(scalarFunction(xInterval.lowerBound, yValue))
+        } else if (zInterval.isEmpty) {
+          forAll(valueWithin(xInterval.intersection(xDomain)), minSuccessful(10)) {
+            (xValue: Double) => {
+              val zValue = scalarFunction(xValue, yValue)
+              assert(zValue.isInfinity || zValue.isNaN)
+            }
+          }
+        } else {
+          val zValues =
+            evaluateWithin(
+              xInterval.intersection(xDomain),
+              xValue => scalarFunction(xValue, yValue)
+            )
+          forAll(zValues, minSuccessful(10)) {
+            (zValue: Double) => {
+              zInterval should approximatelyContain(zValue)
+            }
+          }
+        }
+      }
+    }
+  }
+
+  def testBinaryFunction(
+    scalarFunction: (Double, Double) => Double,
+    intervalFunction: (Interval, Interval) => Interval,
+    xDomain: Interval = Interval.Whole,
+    yDomain: Interval = Interval.Whole
+  ): Unit = {
+    forAll(testIntervalForDomain(xDomain), testIntervalForDomain(yDomain), minSuccessful(50)) {
+      (xInterval: Interval, yInterval: Interval) => {
+        val zInterval = intervalFunction(xInterval, yInterval)
+        if (xInterval.isEmpty || yInterval.isEmpty) {
+          zInterval shouldBe empty
+        } else if (xInterval.isSingleton && yInterval.isSingleton) {
+          val zValue = scalarFunction(xInterval.lowerBound, yInterval.lowerBound)
+          zInterval should approximatelyContain(zValue)
+        } else if (zInterval.isEmpty) {
+          forAll(
+            valueWithin(xInterval.intersection(xDomain)),
+            valueWithin(yInterval.intersection(yDomain)),
+            minSuccessful(10)
+          ) {
+            (xValue: Double, yValue: Double) => {
+              val zValue = scalarFunction(xValue, yValue)
+              assert(zValue.isInfinity || zValue.isNaN)
+            }
+          }
+        } else {
+          val zValues =
+            evaluateWithin(
+              xInterval.intersection(xDomain),
+              yInterval.intersection(yDomain),
+              scalarFunction
+            )
+          forAll(zValues, minSuccessful(10)) {
+            (zValue: Double) => {
+              zInterval should approximatelyContain(zValue)
+            }
+          }
+        }
+      }
+    }
   }
 
   test("equals(other)") {
@@ -118,7 +278,7 @@ class IntervalSuite extends TestSuite {
   }
 
   test("intersection(that)") {
-    forAll(minSuccessful(50)) {
+    forAll(randomInterval, randomInterval, minSuccessful(50)) {
       (firstInterval: Interval, secondInterval: Interval) => {
         val intersection = firstInterval.intersection(secondInterval)
         if (firstInterval.isEmpty || secondInterval.isEmpty) {
@@ -220,5 +380,109 @@ class IntervalSuite extends TestSuite {
       }
       case _ => fail
     }
+  }
+
+  test("unary_-") {
+    testUnaryFunction(value => -value, interval => -interval)
+  }
+
+  test("+(value)") {
+    testMixedFunction((x, y) => x + y, (x, y) => x + y)
+  }
+
+  test("+(that)") {
+    testBinaryFunction((x, y) => x + y, (x, y) => x + y)
+  }
+
+  test("-(value)") {
+    testMixedFunction((x, y) => x - y, (x, y) => x - y)
+  }
+
+  test("-(that)") {
+    testBinaryFunction((x, y) => x - y, (x, y) => x - y)
+  }
+
+  test("*(value)") {
+    testMixedFunction((x, y) => x * y, (x, y) => x * y)
+  }
+
+  test("*(that)") {
+    testBinaryFunction((x, y) => x * y, (x, y) => x * y)
+  }
+
+  test("/(value)") {
+    testMixedFunction((x, y) => x / y, (x, y) => x / y)
+  }
+
+  test("/(that)") {
+    testBinaryFunction((x, y) => x / y, (x, y) => x / y)
+  }
+
+  test("abs") {
+    testUnaryFunction(value => value.abs, interval => interval.abs)
+  }
+
+  test("squared") {
+    testUnaryFunction(value => value * value, interval => interval.squared)
+  }
+
+  test("sqrt") {
+    testUnaryFunction(
+      value => math.sqrt(value),
+      interval => Interval.sqrt(interval),
+      Interval(0.0, Double.PositiveInfinity)
+    )
+  }
+
+  test("sin") {
+    testUnaryFunction(value => math.sin(value), interval => Interval.sin(interval))
+  }
+
+  test("cos") {
+    testUnaryFunction(value => math.cos(value), interval => Interval.cos(interval))
+  }
+
+  test("tan") {
+    testUnaryFunction(value => math.tan(value), interval => Interval.tan(interval))
+  }
+
+  test("asin") {
+    testUnaryFunction(
+      value => math.asin(value),
+      interval => Interval.asin(interval),
+      Interval(-1, 1)
+    )
+  }
+
+  test("acos") {
+    testUnaryFunction(
+      value => math.acos(value),
+      interval => Interval.acos(interval),
+      Interval(-1, 1)
+    )
+  }
+
+  test("atan") {
+    testUnaryFunction(value => math.atan(value), interval => Interval.atan(interval))
+  }
+
+  test("atan2") {
+    testBinaryFunction((y, x) => math.atan2(y, x), (y, x) => Interval.atan2(y, x))
+  }
+
+  test("exp") {
+    testUnaryFunction(
+      value => math.exp(value),
+      interval => Interval.exp(interval),
+      Interval(Double.NegativeInfinity, 1e2)
+    )
+  }
+
+  test("log") {
+    testUnaryFunction(
+      value => math.log(value),
+      interval => Interval.log(interval),
+      Interval(0.0, Double.PositiveInfinity)
+    )
   }
 }
