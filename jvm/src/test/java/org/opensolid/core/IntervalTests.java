f60b81ccda488b448e31b045ad7ba0f60526981d
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2015-12-04 10:45:27 +1100

    Switch to simpler Java 'tests'
    Don't actually test anything, just use each method once to check for
    Scala-Java incompatibilities (and act as a succinct example of how to
    use the OpenSolid API from Java).

--- jvm/src/test/java/org/opensolid/core/IntervalTests.java
+++ jvm/src/test/java/org/opensolid/core/IntervalTests.java
@@ -14,76 +14,74 @@
 
 package org.opensolid.core;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 import scala.Tuple2;
 
 public class IntervalTests {
   @Test
   public void run() {
-    Interval first = new Interval(2, 3);
-    Interval second = new Interval(4, 5);
-    Interval singleton = new Interval(6);
-    assertEquals(first.lowerBound(), 2.0, 1e-12);
-    assertEquals(first.upperBound(), 3.0, 1e-12);
-    assertEquals(first, new Interval(first.lowerBound(), first.upperBound()));
-    assertNotEquals(first, second);
-    assertNotEquals(first.hashCode(), second.hashCode());
-    assertEquals(first.toString(), "Interval(2.0, 3.0)");
+    Interval interval = new Interval(1.0, 3.0);
+    Interval other = new Interval(2.0, 4.0);
+    Interval singleton = new Interval(5.0);
+    Interval empty = Interval.Empty();
+    Interval whole = Interval.Whole();
+    Interval unit = Interval.Unit();
 
-    assertEquals(first.bounds(), first);
-    assertFalse(first.isEmpty());
-    assertFalse(first.isWhole());
-    assertEquals(first.width(), 1.0, 1e-12);
-    assertEquals(first.interpolated(0.5), 2.5, 1e-12);
-    assertEquals(first.median(), 2.5, 1e-12);
+    double lowerBound = interval.lowerBound();
+    double upperBound = interval.upperBound();
+    boolean equal = interval.equals(other);
+    int hashCode = interval.hashCode();
+    String string = interval.toString();
 
-    double randomValue = first.randomValue();
-    assertTrue(randomValue >= first.lowerBound() - 1e-12);
-    assertTrue(randomValue <= first.upperBound() - 1e-12);
+    Interval bounds = interval.bounds();
+    boolean isEmpty = interval.isEmpty();
+    boolean isWhole = interval.isWhole();
+    double width = interval.width();
+    double interpolated = interval.interpolated(0.5);
+    double median = interval.median();
+    double randomValue = interval.randomValue();
+    boolean isSingleton = interval.isSingleton();
 
-    assertFalse(first.isSingleton());
-    assertTrue(singleton.isSingleton());
+    Tuple2<Interval, Interval> bisected = interval.bisected();
+    Interval lowerHalf = bisected._1();
+    Interval upperHalf = bisected._2();
 
-    Tuple2<Interval, Interval> bisected = first.bisected();
-    assertEquals(bisected._1(), new Interval(2, 2.5));
-    assertEquals(bisected._2(), new Interval(2.5, 3));
+    Interval hullValue = interval.hull(3.0);
+    Interval hullInterval = interval.hull(other);
+    Interval intersection = interval.intersection(other);
 
-    assertEquals(first.hull(1), new Interval(1, 3));
-    assertEquals(first.hull(second), new Interval(2, 5));
-    assertEquals(first.intersection(second), Interval.Empty());
+    boolean containsValue = interval.contains(1.0);
+    boolean tolerantContainsValue = interval.contains(1.0, 1e-3);
+    boolean containsInterval = interval.contains(other);
+    boolean tolerantContainsInterval = interval.contains(other, 1e-3);
 
-    assertTrue(Interval.Whole().contains(1e6));
-    assertTrue(first.contains(1.999, 1e-2));
-    assertFalse(first.contains(1.999, 1e-6));
-    assertTrue(first.hull(second).contains(first));
-    assertTrue(first.hull(second).contains(first, 1e-3));
-    assertFalse(first.hull(second).contains(first, -1e-3));
-    assertFalse(first.overlaps(second));
-    assertTrue(first.overlaps(second, 2));
+    boolean overlaps = interval.overlaps(other);
+    boolean tolerantOverlaps = interval.overlaps(other, 1e-3);
 
-    assertEquals(first.negated(), new Interval(-3, -2));
-    assertEquals(first.plus(2), second);
-    assertEquals(first.plus(second), new Interval(6, 8));
-    assertEquals(second.minus(2), first);
-    assertEquals(first.minus(second), new Interval(-3, -1));
-    assertEquals(first.multipliedBy(Sign.Negative()), first.negated());
-    assertEquals(first.multipliedBy(0.5), new Interval(1, 1.5));
-    assertEquals(first.multipliedBy(second), new Interval(8, 15));
-    assertEquals(second.dividedBy(2), new Interval(2, 2.5));
-    Interval quotient = second.dividedBy(first);
-    assertEquals(quotient.lowerBound(), 4.0 / 3.0, 1e-12);
-    assertEquals(quotient.upperBound(), 2.5, 1e-12);
+    Interval negated = interval.negated();
+    Interval plusDouble = interval.plus(1.0);
+    Interval plusInterval = interval.plus(other);
+    Interval minusDouble = interval.minus(1.0);
+    Interval minusInterval = interval.minus(other);
+    Interval timesDouble = interval.times(2.0);
+    Interval timesInterval = interval.times(other);
+    Interval dividedByDouble = interval.dividedBy(2.0);
+    Interval dividedByInterval = interval.dividedBy(other);
 
-    assertEquals(first.minus(3).abs(), Interval.Unit());
-    assertEquals(first.squared(), new Interval(4, 9));
+    Interval abs = interval.abs();
+    Interval squared = interval.squared();
 
-    assertEquals(
-      Interval.sqrt(first),
-      new Interval(Math.sqrt(first.lowerBound()), Math.sqrt(first.upperBound()))
-    );
+    Interval sqrt = Interval.sqrt(interval);
+    Interval sin = Interval.sin(interval);
+    Interval cos = Interval.cos(interval);
+    Interval tan = Interval.tan(interval);
+    Interval asin = Interval.asin(interval);
+    Interval acos = Interval.acos(interval);
+    Interval atan = Interval.atan(interval);
+    Interval atan2 = Interval.atan2(interval, other);
+    Interval exp = Interval.exp(interval);
+    Interval log = Interval.log(interval);
+
+    double ulp = Interval.ulp(interval);
   }
 }
