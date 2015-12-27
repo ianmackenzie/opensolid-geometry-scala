f3d179bdca42a7148c9f7a0b2d931e173a352432
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2015-12-27 19:16:22 +1100

    Update Frame2d to use new cross product function

--- src/main/scala/org/opensolid/core/Frame2d.scala
+++ src/main/scala/org/opensolid/core/Frame2d.scala
@@ -28,8 +28,7 @@ final case class Frame2d(originPoint: Point2d, xDirection: Direction2d, yDirecti
 
   def yAxis: Axis2d = Axis2d(originPoint, yDirection)
 
-  def handedness: Handedness =
-    Handedness.fromSignOf(xDirection.x * yDirection.y - xDirection.y * yDirection.x)
+  def handedness: Handedness = Handedness.fromSignOf(xDirection.cross(yDirection))
 
   def placedOnto(plane: Plane3d): Plane3d =
     Plane3d(
