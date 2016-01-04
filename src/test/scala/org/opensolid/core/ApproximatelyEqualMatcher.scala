43e1dd097ceea72cffc14e6d1ae8c667ae319cc0
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-01-04 13:46:30 +1100

    Add matcher class for approximately-equal tests

---
+++ src/test/scala/org/opensolid/core/ApproximatelyEqualMatcher.scala
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  OpenSolid is a generic library for the representation and manipulation    //
+//  of geometric objects such as points, curves, surfaces, and volumes.       //
+//                                                                            //
+//  Copyright 2007-2015 by Ian Mackenzie                                      //
+//  ian.e.mackenzie@gmail.com                                                 //
+//                                                                            //
+//  This Source Code Form is subject to the terms of the Mozilla Public       //
+//  License, v. 2.0. If a copy of the MPL was not distributed with this file, //
+//  you can obtain one at http://mozilla.org/MPL/2.0/.                        //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+package org.opensolid.core
+
+import scala.math
+
+import org.scalatest._
+import org.scalatest.matchers._
+
+class ApproximatelyEqualMatcher[T](
+  expected: T,
+  tolerance: Double,
+  difference: (T, T) => Double
+) extends Matcher[T] {
+
+  def apply(actual: T): MatchResult = {
+    val error = difference(actual, expected)
+    MatchResult(
+      error <= tolerance,
+      s"$actual is not approximately equal to $expected to within $tolerance (error: $error)",
+      s"$actual is approximately equal to $expected to within $tolerance (error: $error)"
+    )
+  }
+}
