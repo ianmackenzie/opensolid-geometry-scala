22b9b985878bd4f2fcebf601b5a7c19cba1564e2
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-01-07 13:16:57 +1100

    Document Transformable2d.transformedBy

--- src/main/scala/org/opensolid/core/Transformable2d.scala
+++ src/main/scala/org/opensolid/core/Transformable2d.scala
@@ -14,7 +14,36 @@
 
 package org.opensolid.core
 
+/**
+  *
+  */
 trait Transformable2d[T] {
+  /** Returns this object transformed by an arbitrary transformation.
+    *
+    * All other transformation functions simply call this function with the appropriate
+    * `Transformation2d` object. For instance, `point.translatedBy(x, y)` is equivalent to (and
+    * implemented as) `point.transformedBy(Translation2d(x, y))`.
+    *
+    * In most cases it is easier to use the other transformation functions directly. However,
+    * using `transformedBy` is more efficient if the same transformation is applied to many objects,
+    * especially for transformations such as rotations which involve non-trivial initialization. For
+    * instance, the following are equivalent but the second version is faster as it only creates a
+    * single rotation transformation object instead of implicitly creating one for each point in the
+    * list:
+    * {{{
+    * val points: List[Point2d] = ???
+    *
+    * val rotated1 = points.map(_.rotatedAbout(Point2d.Origin, 45.degrees))
+    *
+    * val rotation = Rotation2d(Point2d.Origin, 45.degrees)
+    * val rotated2 = points.map(_.transformedBy(rotation))
+    * }}}
+    * In this case, however, it would be simpler to exploit the fact that Transformation2d objects
+    * can be used as functions:
+    * {{{
+    * val rotated3 = points.map(rotation)
+    * }}}
+    */
   def transformedBy(transformation: Transformation2d): T
 
   def translatedBy(vector: Vector2d): T = transformedBy(Translation2d(vector))
