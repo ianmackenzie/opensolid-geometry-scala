52e57d3b5522209252cf09fa7aab06ff6170e652
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-01-25 20:19:38 +1100

    Add Curve2d

---
+++ src/main/scala/org/opensolid/core/Curve2d.scala
@@ -0,0 +1,72 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  OpenSolid is a generic library for the representation and manipulation    //
+//  of geometric objects such as points, curves, surfaces, and volumes.       //
+//                                                                            //
+//  Copyright 2007-2015 by Ian Mackenzie                                      //
+//  ian.e.mackenzie@gmail.com                                                 //
+//                                                                            //
+//  This Source Code Form is subject to the terms of the Mozilla Public       //
+//  License, v. 2.0. If a copy of the MPL was not distributed with this file, //
+//  you can obtain one at http://mozilla.org/MPL/2.0/.                        //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+package org.opensolid.core
+
+trait Curve2d {
+  import Curve2d._
+
+  def expression: Expression
+
+  def domain: Interval
+
+  def evaluate(parameterValue: Double): Point2d
+
+  def bounds(parameterBounds: Interval): Box2d
+}
+
+object Curve2d {
+  type Expression = Expression2d[Double]
+
+  def apply(expression: Expression, domain: Interval): Curve2d = parametric(expression, domain)
+
+  def parametric(expression: Expression, domain: Interval): Curve2d = Parametric(expression, domain)
+
+  private case class Parametric(expression: Expression, domain: Interval) extends Curve2d {
+    private[this] val (evaluationSequence, (xIndex, yIndex)) =
+      EvaluationSequence.evaluate2d(expression)
+
+    override def evaluate(parameterValue: Double): Point2d = {
+      val array = Array.ofDim[Double](evaluationSequence.arraySize)
+      array(0) = parameterValue
+      for (operation <- evaluationSequence.operations) {
+        operation.execute(array)
+      }
+      Point2d(array(xIndex), array(yIndex))
+    }
+
+    override def bounds(parameterBounds: Interval): Box2d = {
+      val array = Array.ofDim[Interval](evaluationSequence.arraySize)
+      array(0) = parameterBounds
+      for (operation <- evaluationSequence.operations) {
+        operation.execute(array)
+      }
+      Box2d(array(xIndex), array(yIndex))
+    }
+  }
+
+  def constant(point: Point2d): Curve2d = Constant(point)
+
+  private case class Constant(point: Point2d) extends Curve2d {
+    private[this] val box = Box2d.singleton(point)
+
+    override def expression: Expression = Expression2d.Constant(point.x, point.y)
+
+    override def domain: Interval = Interval.Whole
+
+    override def evaluate(parameterValue: Double): Point2d = point
+
+    override def bounds(parameterBounds: Interval): Box2d = box
+  }
+}
