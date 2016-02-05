88237cc2d3b8be3004e202776c2f202906d4d24a
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-02-05 17:01:41 +1100

    Switch to method definitions on their own line
    Also add multiplication overloads for scalars and vector bounds, and
    scalars and direction bounds

--- src/main/scala/org/opensolid/core/package.scala
+++ src/main/scala/org/opensolid/core/package.scala
@@ -84,67 +84,92 @@ import scala.math
   */
 package object core {
   implicit class ImplicitScalar(val value: Double) extends AnyVal {
-    def isZero(tolerance: Double): Boolean = value >= -tolerance && value <= tolerance
+    def isZero(tolerance: Double): Boolean =
+      value >= -tolerance && value <= tolerance
 
-    def isNotZero(tolerance: Double): Boolean = value < -tolerance || value > tolerance
+    def isNotZero(tolerance: Double): Boolean =
+      value < -tolerance || value > tolerance
 
-    def meters: Double = value
+    def meters: Double =
+      value
 
-    def inMeters: Double = value
+    def inMeters: Double =
+      value
 
-    def centimeters: Double = value * 1e-2
+    def centimeters: Double =
+      value * 1e-2
 
-    def inCentimeters: Double = value * 1e2
+    def inCentimeters: Double =
+      value * 1e2
 
-    def millimeters: Double = value * 1e-3
+    def millimeters: Double =
+      value * 1e-3
 
-    def inMillimeters: Double = value * 1e3
+    def inMillimeters: Double =
+      value * 1e3
 
-    def microns: Double = value * 1e-6
+    def microns: Double =
+      value * 1e-6
 
-    def inMicrons: Double = value * 1e6
+    def inMicrons: Double =
+      value * 1e6
 
-    def kilometers: Double = value * 1e3
+    def kilometers: Double =
+      value * 1e3
 
-    def inKilometers: Double = value * 1e-3
+    def inKilometers: Double =
+      value * 1e-3
 
-    def inches: Double = value * 0.0254
+    def inches: Double =
+      value * 0.0254
 
-    def inInches: Double = value / 0.0254
+    def inInches: Double =
+      value / 0.0254
 
-    def feet: Double = value * 0.3048
+    def feet: Double =
+      value * 0.3048
 
-    def inFeet: Double = value / 0.3048
+    def inFeet: Double =
+      value / 0.3048
 
-    def thou: Double = value * 0.0000254
+    def thou: Double =
+      value * 0.0000254
 
-    def inThou: Double = value / 0.0000254
+    def inThou: Double =
+      value / 0.0000254
 
-    def yards: Double = value * 0.9144
+    def yards: Double =
+      value * 0.9144
 
-    def inYards: Double = value / 0.9144
+    def inYards: Double =
+      value / 0.9144
 
-    def miles: Double = value * 1609.344
+    def miles: Double =
+      value * 1609.344
 
-    def inMiles: Double = value / 1609.344
+    def inMiles: Double =
+      value / 1609.344
 
-    def radians: Double = value
+    def radians: Double =
+      value
 
-    def inRadians: Double = value
+    def inRadians: Double =
+      value
 
-    def degrees: Double = math.toRadians(value)
+    def degrees: Double =
+      math.toRadians(value)
 
-    def inDegrees: Double = math.toDegrees(value)
+    def inDegrees: Double =
+      math.toDegrees(value)
 
-    def +(interval: Interval): Interval = {
-      Interval(value + interval.lowerBound, value + interval.upperBound)
-    }
+    def +(interval: Interval): Interval =
+      interval + value
 
-    def -(interval: Interval): Interval = {
+    def -(interval: Interval): Interval =
       Interval(value - interval.upperBound, value - interval.lowerBound)
-    }
 
-    def *(interval: Interval): Interval = interval * value
+    def *(interval: Interval): Interval =
+      interval * value
 
     def /(interval: Interval): Interval = {
       if (interval.isEmpty) {
@@ -168,13 +193,29 @@ package object core {
       }
     }
 
-    def *(vector: Vector2d): Vector2d = vector * value
+    def *(vector: Vector2d): Vector2d =
+      vector * value
 
-    def *(vector: Vector3d): Vector3d = vector * value
+    def *(vector: Vector3d): Vector3d =
+      vector * value
 
-    def *(direction: Direction2d): Vector2d = direction * value
+    def *(direction: Direction2d): Vector2d =
+      direction * value
 
-    def *(direction: Direction3d): Vector3d = direction * value
+    def *(direction: Direction3d): Vector3d =
+      direction * value
+
+    def *(vectorBounds: VectorBounds2d): VectorBounds2d =
+      vectorBounds * value
+
+    def *(vectorBounds: VectorBounds3d): VectorBounds3d =
+      vectorBounds * value
+
+    def *(directionBounds: DirectionBounds2d): VectorBounds2d =
+      directionBounds * value
+
+    def *(directionBounds: DirectionBounds3d): VectorBounds3d =
+      directionBounds * value
 
     def +[P](scalarExpression: ScalarExpression[P]): ScalarExpression[P] =
       ScalarExpression.Constant[P](value) + scalarExpression
