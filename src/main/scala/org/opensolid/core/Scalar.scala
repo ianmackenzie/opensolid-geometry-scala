d4c45c7f6983d7cbf9b9d0557516caec1a883a02
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2016-02-09 10:49:44 +1100

    Refactor Scalar/Meters
    Merge into Scalar class that is a direct mirror of DoubleExtensions:
    slightly less efficient since now Scalar objects must be instantiated,
    but much cleaner

--- src/main/scala/org/opensolid/core/Scalar.scala
+++ src/main/scala/org/opensolid/core/Scalar.scala
@@ -14,19 +14,139 @@
 
 package org.opensolid.core
 
-object Scalar {
-  def isZero(value: Double, tolerance: Double): Boolean =
+class Scalar(val value: Double) extends AnyVal {
+  def isZero(tolerance: Double): Boolean =
     value.isZero(tolerance)
 
-  def isNotZero(value: Double, tolerance: Double): Boolean =
+  def isNotZero(tolerance: Double): Boolean =
     value.isNotZero(tolerance)
 
-  def difference(value: Double, interval: Interval): Interval  =
+  def meters: Double =
+    value.meters
+
+  def inMeters: Double =
+    value.inMeters
+
+  def centimeters: Double =
+    value.centimeters
+
+  def inCentimeters: Double =
+    value.inCentimeters
+
+  def millimeters: Double =
+    value.millimeters
+
+  def inMillimeters: Double =
+    value.inMillimeters
+
+  def microns: Double =
+    value.microns
+
+  def inMicrons: Double =
+    value.inMicrons
+
+  def kilometers: Double =
+    value.kilometers
+
+  def inKilometers: Double =
+    value.inKilometers
+
+  def inches: Double =
+    value.inches
+
+  def inInches: Double =
+    value.inInches
+
+  def feet: Double =
+    value.feet
+
+  def inFeet: Double =
+    value.inFeet
+
+  def thou: Double =
+    value.thou
+
+  def inThou: Double =
+    value.inThou
+
+  def yards: Double =
+    value.yards
+
+  def inYards: Double =
+    value.inYards
+
+  def miles: Double =
+    value.miles
+
+  def inMiles: Double =
+    value.inMiles
+
+  def radians: Double =
+    value.radians
+
+  def inRadians: Double =
+    value.inRadians
+
+  def degrees: Double =
+    value.degrees
+
+  def inDegrees: Double =
+    value.inDegrees
+
+  def plus(interval: Interval): Interval =
+    value + interval
+
+  def minus(interval: Interval): Interval =
     value - interval
 
-  def quotient(value: Double, interval: Interval): Interval =
+  def times(interval: Interval): Interval =
+    value * interval
+
+  def dividedBy(interval: Interval): Interval =
     value / interval
 
-  def hull(firstValue: Double, secondValue: Double): Interval =
-    firstValue.hull(secondValue)
+  def times(vector: Vector2d): Vector2d =
+    value * vector
+
+  def times(vector: Vector3d): Vector3d =
+    value * vector
+
+  def times(direction: Direction2d): Vector2d =
+    value * direction
+
+  def times(direction: Direction3d): Vector3d =
+    value * direction
+
+  def times(vectorBounds: VectorBounds2d): VectorBounds2d =
+    value * vectorBounds
+
+  def times(vectorBounds: VectorBounds3d): VectorBounds3d =
+    value * vectorBounds
+
+  def times(directionBounds: DirectionBounds2d): VectorBounds2d =
+    value * directionBounds
+
+  def times(directionBounds: DirectionBounds3d): VectorBounds3d =
+    value * directionBounds
+
+  def plus[P](scalarExpression: ScalarExpression[P]): ScalarExpression[P] =
+    value + scalarExpression
+
+  def minus[P](scalarExpression: ScalarExpression[P]): ScalarExpression[P] =
+    value - scalarExpression
+
+  def times[P](scalarExpression: ScalarExpression[P]): ScalarExpression[P] =
+    value * scalarExpression
+
+  def dividedBy[P](scalarExpression: ScalarExpression[P]): ScalarExpression[P] =
+    value / scalarExpression
+
+  def times[P](vectorExpression: VectorExpression2d[P]): VectorExpression2d[P] =
+    value * vectorExpression
+
+  def hull(that: Double): Interval =
+    value.hull(that)
+
+  def hull(interval: Interval): Interval =
+    value.hull(interval)
 }
